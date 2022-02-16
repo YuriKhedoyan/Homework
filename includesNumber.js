cebc1939422148a1f90d14a294debc02c2726e47
YuriKhedoyan <yurakhedoyan@gmail.com>
2022-02-16 22:05:50 +0400

    Modified includesNumber.js

--- includesNumber.js
+++ includesNumber.js
@@ -1,10 +1,5 @@
-function includesNumber(num) {
+const includesNumber = num => {
   const arr = String(num).split("");
-
-  for (let i = 0; i < arr.length; i++) {
-      if (arr[i] === "7" && arr.slice(i + 1).includes("7")) {
-          return true;
-      }
-  }
-  return false;
+  
+  return arr.some((el, i) => { return el === "7" && arr.slice(i + 1).includes("7") })
 }
