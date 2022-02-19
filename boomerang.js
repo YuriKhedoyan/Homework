683bfac0c9a2347c4a49319cc752d842342f4a60
YuriKhedoyan <yurakhedoyan@gmail.com>
2022-02-19 23:44:19 +0400

    Modified boomerang.js

--- boomerang.js
+++ boomerang.js
@@ -1,9 +1,7 @@
-const boomerang = arr => {
-  return arr.reduce((acum, el, i, array) => {
+const boomerang = arr => arr.reduce((acum, el, i, array) => {
     if (array[i] === array[i + 2]) {
       acum.push(array.slice(i, i + 3));
     }
 
     return acum;
   }, [])
-}
