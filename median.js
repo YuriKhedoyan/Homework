5e3f0b50c0ef9708a5a20ba6904adb9365f29cda
YuriKhedoyan <yurakhedoyan@gmail.com>
2022-03-04 19:30:06 +0400

    Modified median.js

--- median.js
+++ median.js
@@ -1,3 +1,1 @@
-const median = arr => {
-  return arr.sort((a, b) => a-b).length % 2 !== 0 ? arr[Math.floor(arr.length / 2)] : (arr[Math.floor(arr.length / 2) - 1]);
-}
+const median = arr => arr.sort((a, b) => a-b).length % 2 !== 0 ? arr[Math.floor(arr.length / 2)] : (arr[Math.floor(arr.length / 2) - 1]);
