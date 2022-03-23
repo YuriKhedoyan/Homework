47332fa3dabd58d6f5f88a36ae3931ae5ee27189
YuriKhedoyan <yurakhedoyan@gmail.com>
2022-03-23 17:20:13 +0400

    Modified median.js

--- median.js
+++ median.js
@@ -1,4 +1,5 @@
 const median = (arr1, arr2) => {
-  const clone = arr1.concat(arr2).sort((a,b) => a - b);
-  return clone.length % 2 === 0 ? clone[clone.length / 2] : clone[Math.floor(clone.length / 2)];
+  const sortedArray = arr1.concat(arr2).sort((a,b) => a - b);
+  const index = clone.length / 2;
+  return sortedArray.length % 2 === 0 ? sortedArray[index] : sortedArray[Math.floor(index)];
 }
