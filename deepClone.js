8d9d863ab9570d20fd137b91713725552854ab65
YuriKhedoyan <yurakhedoyan@gmail.com>
2022-04-04 18:03:16 +0400

    Modified deepClone.js

--- deepClone.js
+++ deepClone.js
@@ -1,11 +1,13 @@
-const copyObject = obj => {
+const deepClone = obj => {
   const copiedObj = {};
-
+    
   for (let key in obj) {
-    copiedObj[key] = obj[key];
+    if (typeof obj[key] === "object") {
+        copiedObj[key] = deepClone(key);
+    } else {
+        copiedObj[key] = obj[key];
+    }
   }
-
+  
   return copiedObj;
-};
-
-const deepClone = obj => JSON.parse(JSON.stringify(obj));
+}
