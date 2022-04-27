fc759a1f9ea85bbdbbbdee88d5eb2a1c4c5191fc
YuriKhedoyan <yurakhedoyan@gmail.com>
2022-04-27 22:52:44 +0400

    Modified reverseInt.js

--- reverseInt.js
+++ reverseInt.js
@@ -1,17 +1,16 @@
-const reverse = function(x) {
+const reverse = function (x) {
   const arrayNumbers = String(x).split("");
-  const index = arrayNumbers.findIndex((el, i) => el === "-");
   let result;
 
-  if (index === -1) {
-      result = Number(arrayNumbers.reverse().join(""));
+  if (arrayNumbers[0] === "-") {
+    result = -Number(arrayNumbers.slice(1).reverse().join(""));
   } else {
-      result = Number("-" + arrayNumbers.slice(index + 1).reverse().join(""));
+    result = Number(arrayNumbers.reverse().join(""));
   }
-
-  if (2147483647  > Math.abs(result)) {
-      return result 
+  console.log(result < 2 ** 32)
+  if (2 ** 32 < Math.abs(result)) {
+    return 0
   } else {
-      return 0
+    return result
   }
 };
