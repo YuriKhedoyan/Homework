1e25d7d63a40adbf3fbd450c15c7a37d6028c19f
YuriKhedoyan <yurakhedoyan@gmail.com>
2022-04-28 18:15:16 +0400

    Modified mathematicOperators.js

--- mathematicOperators.js
+++ mathematicOperators.js
@@ -1,6 +1,6 @@
 const mathematicOperators = (arr, num) => {
   return arr.some((el, ind) => {
-    return arr.slice(ind + 1).some((element, i) => {
+    return arr.slice(ind + 1).some(element => {
       if (el + element === num || el - element === num || el / element === num || el * element === num) {
         return true
       }
