8d52dc00f5ba179e6a43e282dd3c9af401e985e5
YuriKhedoyan <yurakhedoyan@gmail.com>
2022-05-23 13:12:24 +0400

    Add linked list crud

---
+++ linkedListCrud.js
@@ -0,0 +1,49 @@
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.lastElement = null;
+  }
+
+  createElem(data) {
+    const node = { data, next: null }
+
+    if (this.lastElement) {
+      this.lastElement.next = node;
+    }
+
+    if (!this.head) {
+      this.head = node;
+    }
+
+    this.lastElement = node;
+  }
+
+  update(data, newData) {
+    let current = this.head;
+
+    while (current.next) {
+      if (current.data === data) {
+        current.data = newData;
+      }
+
+      current = current.next;
+    }
+  }
+
+  delete(data) {
+    let current = this.head;
+
+    if (current.next) {
+      while (current.next) {
+        if (current.next.data === data) {
+          current.next = current.next.next;
+        }
+
+        current = current.next;
+      }
+    } else {
+      this.head = null;
+      this.lastElement = null;
+    }
+  }
+}
