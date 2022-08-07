66368d535439d018411b7b94ce14f12a9b10d2b5
Yuri Khedoyan <yurakhedoyan@gmail.com>
2022-08-07 22:55:35 +0400

    Add Binary Search whit recursia

--- binarySearch.js
+++ binarySearch.js
@@ -15,3 +15,22 @@ const binarySearch = (numbers, searchNumber) => {
     }
   }
 }
+
+const binarySearchWhitRecurisa = arr => {
+  const initialArray = arr;
+
+  return function search(searchElement, array=arr) {
+      const max = array[array.length - 1];
+      const min = array[0];
+      
+      if (min > max || arr.length - 1 < searchElement) return -1;
+      
+      const mid = Math.floor(array.length / 2);
+      
+      if (array[mid] === searchElement) return initialArray.findIndex(el => el === array[mid]);
+      if (searchElement > array[mid]) {
+          return search(searchElement, array.slice(mid));
+      } else {
+          return search(searchElement, array.slice(0, mid));
+      }
+}}
